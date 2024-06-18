f951a93c0e0905027927f94483e622e90fd56bd9
WolverinDEV <git@did.science>
2024-06-18 13:33:19 +0200

    Fixed the incorrect GitHub link in the footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
                         },
                         {
                             label: 'GitHub',
-                            href: 'https://github.com/facebook/docusaurus',
+                            href: 'https://github.com/Valthrun/Valthrun',
                         },
                     ],
                 },
